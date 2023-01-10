f05300c71c46bec4d9fa065defa1b3696b87d222
simonechebelnome <mennasimone16@gmail.com>
2023-01-10 12:48:45 +0100

    Started Working on json file creation

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
+const fs = require('fs');
+const generator = require('@babel/generator');
+const { parse } = require("@babel/parser");
+const { getTypeScriptReader, getOpenApiWriter, makeConverter } = require('typeconv');
+const traverse = require('@babel/traverse').default;
 class TypescriptToSwagger {
-
     getAst(url: string): any {
-        const fs = require('fs');
-        const { parse } = require("@babel/parser");
         const fileContents = fs.readFileSync(url).toString();
-
         return parse(fileContents, { sourceType: 'module', plugins: ["typescript"] });
     }
 
     searchInterestingNodes(ast: any, keyword: string, nodeType: string): Node[] {
-        const traverse = require('@babel/traverse').default;
         const nodes: Node[] = [];
 
         traverse(ast, {
@@ -28,12 +28,10 @@ class TypescriptToSwagger {
     }
 
     nodesToTypescript(nodes: Node[]): string {
-        const generator = require('@babel/generator');
         return nodes.map(node => `export ${generator.default(node).code}`).join('\n');
     }
 
     typescriptToOpenApiJson(code: string, apiName: string, version: string): Promise<any> {
-        const { getTypeScriptReader, getOpenApiWriter, makeConverter } = require('typeconv');
         const reader = getTypeScriptReader();
         const writer = getOpenApiWriter({ format: 'json', title: apiName, version });
         const { convert } = makeConverter(reader, writer);
@@ -46,13 +44,27 @@ class TypescriptToSwagger {
 
     }
 
-    // return promise
     generateSwagger(url: string, apiName: string, version: string): Promise<any> {
         const ast = this.getAst(url);
         const nodes = this.searchInterestingNodes(ast, 'swagger', 'TSInterfaceDeclaration');
         const code = this.nodesToTypescript(nodes);
         return this.typescriptToOpenApiJson(code, apiName, version);
     }
-
-
+    
+    createJson(json: Object, fileName: string) {
+        const convertedJson = JSON.stringify(json);
+        fs.writeFile(fileName, convertedJson, (err: Error) => {
+            if(err)
+                console.error(err.message);
+        })
+    }
 }
+
+const json = new TypescriptToSwagger();
+const swagger = json.generateSwagger('test/test.ts', 'Person API', 'v1').then(result => console.log(result));
+
+// json.createJson({
+//     prova: 'ciao',
+//     id: 5
+// }, 'swagger.json');
+
---
+++ test/test.ts
@@ -0,0 +1,5 @@
+export interface Person {
+    id: string,
+    user: string,
+    age: number
+}
