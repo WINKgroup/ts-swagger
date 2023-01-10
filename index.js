db093aa7fba35faf4eb8d99a9342ed8bfd33fc78
simonechebelnome <mennasimone16@gmail.com>
2023-01-10 15:26:13 +0100

    deleted test

--- index.js
+++
@@ -1,6 +0,0 @@
-const myClass = require('./src/index.ts');
-
-const json = new myClass();
-
-json.generateSwagger('src/test.ts', 'Person API', 'v1').then(
-    result => json.createJson(result.data, 'swagger.json'));
--- src/test.ts
+++
@@ -1,5 +0,0 @@
-export interface Person {
-    // swagger
-    id: string;
-    age: number;
-}
