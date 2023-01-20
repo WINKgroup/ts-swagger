796001a8d939508cb2471bf7e49ff1ac49b9e1be
alexSp84 <alessandro.spinelli84@outlook.it>
2023-01-20 16:58:07 +0100

    refactor

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,12 @@ import * as parser from "@babel/parser";
 import traverse from "@babel/traverse";
 import * as t from '@babel/types';
 import { JSONFileError, JSONPathError, ApiPathError } from "./_errors";
+import {
+  checkMethodName,
+  getMethodInfo,
+  getVariableType,
+  isDate
+} from "./utils";
 
 class TsSwagger {
   readonly tsSwgConfig: TsSwgConfig;
@@ -59,94 +65,6 @@ class TsSwagger {
 
   scanExpressApi(ast: parser.ParseResult<t.File>) {
     const methods: TsSwgMethod[] = [];
-
-    const getResStatus = (comment: string) => {
-      const status = comment.match(/\{(.*?)\}/);
-      if (status) {
-        return {
-          [status[1]]: {
-            description: getCommentValue(`{${status[1]}}`, comment, true) || ''
-          }
-        }
-      }
-      return;
-    };
-
-    const getCommentValue = (label: string, comment: string, spacing?: boolean) => {
-      const result = comment.split(label)[1];
-
-      return spacing ? result : result.replace(/\s+/g, "");
-    };
-
-    const getQueryParameter = (comment: string) => {
-      const name = comment.match(/\{(.*?)\}/);
-      const type = comment.match(/\[(.*?)\]/);
-
-      if (name && type) {
-        return {
-          name: name[1],
-          type: type[1],
-          description: getCommentValue(`[${type[1]}]`, comment, true) || ''
-        }
-      }
-    };
-
-    const getMethodInfo = (rows: string[]) => {
-      let methodInfo: TsSwgMethod = { name: "", path: "", queryParameters: [] };
-      for (const index in rows) {
-        const row = rows[index];
-        if (row.includes("@schema"))
-          methodInfo = {
-            ...methodInfo,
-            interfaceName: getCommentValue("@schema ", row)
-          };
-        else if (row.includes("@request"))
-          methodInfo = {
-            ...methodInfo,
-            name: getCommentValue("@request ", row).toLowerCase()
-          };
-        else if (row.includes("@route"))
-          methodInfo = {
-            ...methodInfo,
-            path: getCommentValue("@route ", row).toLowerCase()
-          };
-        else if (row.includes("@description"))
-          methodInfo = {
-            ...methodInfo,
-            description: getCommentValue("@description ", row, true)
-          };
-        else if (row.includes("@response_description"))
-          methodInfo = {
-            ...methodInfo,
-            responseDescription: getCommentValue("@response_description ", row, true)
-          };
-        else if (row.includes("@error_schema"))
-          methodInfo = {
-            ...methodInfo,
-            errorInterfaceName: getCommentValue("@error_schema ", row)
-          };
-        else if (row.includes("@status_code"))
-          _.merge(methodInfo, { resStates: getResStatus(row) });
-        else if (row.includes("@query_parameter")) {
-          const queryParameter = getQueryParameter(row);
-          queryParameter && methodInfo.queryParameters?.push(queryParameter);
-        }
-      }
-
-      return methodInfo;
-    };
-
-    const checkMethodName = (name?: string) => {
-      switch (name) {
-        case "get":
-        case "put":
-        case "post":
-        case "delete":
-          return true
-        default:
-          return false
-      }
-    };
 
     traverse(ast, {
       enter(path: any) {
@@ -289,37 +207,6 @@ class TsSwagger {
 
   getDataForSchemas(nodes: t.TSInterfaceDeclaration[]): TsSwgSchemasData[] {
     const schemasData: TsSwgSchemasData[] = [];
-
-    const getVariableType = (type: string) => {
-      switch (type) {
-        case "TSBooleanKeyword":
-          return "boolean"
-        case "TSNumberKeyword":
-          return "number"
-        case "TSArrayType":
-          return "array"
-        case "TSObjectKeyword":
-          return "object"
-        default:
-          return "string"
-      }
-    };
-
-    const isDate = (body: t.TSTypeElement) => {
-      return (
-        body.typeAnnotation?.typeAnnotation.type === "TSTypeReference" &&
-        "typeName" in body.typeAnnotation?.typeAnnotation &&
-        "name" in body.typeAnnotation?.typeAnnotation.typeName &&
-        body.typeAnnotation?.typeAnnotation.typeName.name === "Date"
-      ) ||
-        (
-          body.typeAnnotation?.typeAnnotation &&
-          "elementType" in body.typeAnnotation?.typeAnnotation &&
-          "typeName" in body.typeAnnotation?.typeAnnotation.elementType &&
-          "name" in body.typeAnnotation?.typeAnnotation.elementType.typeName &&
-          body.typeAnnotation?.typeAnnotation.elementType.typeName.name === "Date"
-        )
-    }
 
     nodes.forEach(node => {
       const variables: TsSwgVariable[] = [];
